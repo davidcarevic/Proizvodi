d636b0371a96429dcb6182b87a12fcbda35d31ae
david.carevic.biz@gmail.com <67685468+DavidCarevic7@users.noreply.github.com>
2020-12-03 14:54:02 +0100

    Application layer initial

---
+++ Application/DTO/EditKategorijaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTO
+{
+    public class EditKategorijaDTO
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+       
+    }
+}
---
+++ EFDataAccess/Migrations/20201202145803_Initial Tables.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFDataAccess.Migrations
+{
+    public partial class InitialTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dobavljacs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dobavljacs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kategorijas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kategorijas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Proizvodjacs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proizvodjacs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Proizvods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Opis = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Cena = table.Column<double>(type: "float", nullable: false),
+                    KatId = table.Column<int>(type: "int", nullable: false),
+                    ProId = table.Column<int>(type: "int", nullable: false),
+                    DobId = table.Column<int>(type: "int", nullable: false),
+                    KategorijaId = table.Column<int>(type: "int", nullable: true),
+                    DobavljacId = table.Column<int>(type: "int", nullable: true),
+                    ProizvodjacId = table.Column<int>(type: "int", nullable: true),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proizvods", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Proizvods_Dobavljacs_DobavljacId",
+                        column: x => x.DobavljacId,
+                        principalTable: "Dobavljacs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Proizvods_Kategorijas_KategorijaId",
+                        column: x => x.KategorijaId,
+                        principalTable: "Kategorijas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Proizvods_Proizvodjacs_ProizvodjacId",
+                        column: x => x.ProizvodjacId,
+                        principalTable: "Proizvodjacs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Proizvods_DobavljacId",
+                table: "Proizvods",
+                column: "DobavljacId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Proizvods_KategorijaId",
+                table: "Proizvods",
+                column: "KategorijaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Proizvods_ProizvodjacId",
+                table: "Proizvods",
+                column: "ProizvodjacId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Proizvods");
+
+            migrationBuilder.DropTable(
+                name: "Dobavljacs");
+
+            migrationBuilder.DropTable(
+                name: "Kategorijas");
+
+            migrationBuilder.DropTable(
+                name: "Proizvodjacs");
+        }
+    }
+}
